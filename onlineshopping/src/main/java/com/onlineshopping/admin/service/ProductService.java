3951e7d3053917e9f3a71d8a11d8320ae0876ff3
VALKKUMA <VALKKUMA@LIN51017056.corp.capgemini.com>
2021-11-15 15:04:24 +0530

    this is test

--- onlineshopping/src/main/java/com/onlineshopping/admin/service/ProductService.java
+++ onlineshopping/src/main/java/com/onlineshopping/admin/service/ProductService.java
@@ -1,5 +1,16 @@
-package com.onlineshopping.admin.service;
+import java.util.List;
+
+import com.onlineshopping.adminentity.Product;
 
 public interface ProductService {
+	Product addProduct(Product product);
+
+	Product updateProduct(Product product);
+
+	List<Product> getAllproduct();
+
+	Product getProductById(long productId);
+
+	void deleteProduct(long productId);
 
 }
--- onlineshopping/src/main/java/com/onlineshopping/admin/service/ProductServiceImpl.java
+++ onlineshopping/src/main/java/com/onlineshopping/admin/service/ProductServiceImpl.java
@@ -1,5 +1,68 @@
-package com.onlineshopping.admin.service;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.onlineshopping.admindao.ProductRepository;
+import com.onlineshopping.adminentity.Product;
+import com.onlineshopping.adminexception.ProductNotFoundException;
+
+@Service
+@Transactional
 public class ProductServiceImpl implements ProductService {
 
+	@Autowired
+	private ProductRepository repository;
+
+	@Override
+	public Product addProduct(Product product) {
+// TODO Auto-generated method stub
+		return repository.save(product);
+	}
+
+	@Override
+	public Product updateProduct(Product product) {
+// TODO Auto-generated method stub
+		Optional<Product> productDb = this.repository.findById(product.getId());
+
+		if (productDb.isPresent()) {
+			Product productUpdate = productDb.get();
+			productUpdate.setId(product.getId());
+			productUpdate.setName(product.getName());
+			productUpdate.setPrice(product.getPrice());
+			return productUpdate;
+		} else {
+			throw new ProductNotFoundException("product not found with id :" + product.getId());
+		}
+	}
+
+	@Override
+	public List<Product> getAllproduct() {
+// TODO Auto-generated method stub
+		return (List<Product>) this.repository.findAll();
+	}
+
+	@Override
+	public Product getProductById(long productId) {
+// TODO Auto-generated method stub
+		Optional<Product> productDb = this.repository.findById(productId);
+		if (productDb.isPresent()) {
+			return productDb.get();
+		} else {
+			throw new ProductNotFoundException("product not found with id :" + productId);
+		}
+	}
+
+	@Override
+	public void deleteProduct(long productId) {
+// TODO Auto-generated method stub
+		Optional<Product> productDb = this.repository.findById(productId);
+		if (productDb.isPresent()) {
+			this.repository.delete(productDb.get());
+		} else {
+			throw new ProductNotFoundException("product not found with id :" + productId);
+		}
+	}
 }
